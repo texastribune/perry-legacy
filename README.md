99b80ada6002be6821ff1418f7ad3b0f04857715
Becca Aaronson <becca.aaronson@gmail.com>
2014-11-17 16:08:18 -0600

    add tools to process spreadsheet

--- README.md
+++ README.md
@@ -2,3 +2,11 @@ Perry Palooza
 =============
 
 A multi-part feature on the legacy Gov. Rick Perry
+
+
+Node modules that you'll need to install to process speadsheet:
++googleapis
++request
++untildify
++xlsx
++marked
---
+++ tools/build_data_json.js
@@ -0,0 +1,33 @@
+var fs = require('fs');
+var marked = require('marked');
+var XLSX = require('xlsx');
+
+marked.setOptions({
+  smartypants: true
+});
+
+var SHEETS = [
+  'Stories'
+];
+
+var DATA = {};
+
+var workbook = XLSX.readFile('data.xlsx');
+
+SHEETS.forEach(function(sheet) {
+  'use strict';
+
+  var worksheet = workbook.Sheets[sheet];
+  // var test = XLSX.utils.sheet_to_json(worksheet);
+
+  // test.forEach(function(e) {
+  //   if (!e['Bio']) {
+  //     return;
+  //   }
+  //   console.log(marked(e['Bio']));
+  // });
+
+  DATA[sheet] = XLSX.utils.sheet_to_json(worksheet);
+});
+
+fs.writeFileSync('data.json', JSON.stringify(DATA, null, 2));
