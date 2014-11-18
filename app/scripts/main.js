a6897fe41bb064679e08a53eaa6a7f14e2872c1d
Becca Aaronson <becca.aaronson@gmail.com>
2014-11-18 12:11:49 -0600

    edit masthead/banner styles

---
+++ app/scripts/main.js
@@ -0,0 +1,18 @@
+// Set element height based on height of reference element
+function setHeight(target, reference) {
+  var refHeight = $(reference).innerHeight();
+  $(target).css('height', refHeight);
+}
+
+$(document).ready(function() {
+  setHeight('.banner', window);
+});
+
+// Reset heights/positions on window resize
+$(window).resize(function() {
+  setHeight('.landing-banner', window);
+});
+
+$(window).on( "orientationchange", function() {
+  setHeight('.landing-banner', window);
+});
