41a3e64bffea32f6f7b22ac0c3853cff1052a97f
Becca Aaronson <becca.aaronson@gmail.com>
2014-11-24 13:07:02 -0600

    remove mmenu, add starter-menu basic functionality

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,4 +1,5 @@
 // Set element height based on height of reference element
+
 function setHeight(target, reference) {
   var refHeight = $(reference).innerHeight();
   $(target).css('height', refHeight);
@@ -14,10 +15,39 @@ $(window).resize(function() {
   setHeight('.banner', window);
 });
 
-$(window).on( "orientationchange", function() {
+$(window).on( 'orientationchange', function() {
   setHeight('.banner', window);
 });
 
-// $("#menu").click(function(){
-//   $(".menu-icon").toggleClass("x");
-// });
+(function () {
+  'use strict';
+
+  var querySelector = document.querySelector.bind(document);
+
+  var navdrawerContainer = querySelector('.navdrawer-container');
+  var body = document.body;
+  var appbarElement = querySelector('.app-bar');
+  var menuBtn = querySelector('.menu');
+  var main = querySelector('.main');
+
+  function closeMenu() {
+    body.classList.remove('open');
+    appbarElement.classList.remove('open');
+    navdrawerContainer.classList.remove('open');
+  }
+
+  function toggleMenu() {
+    body.classList.toggle('open');
+    appbarElement.classList.toggle('open');
+    navdrawerContainer.classList.toggle('open');
+    navdrawerContainer.classList.add('opened');
+  }
+
+  main.addEventListener('click', closeMenu);
+  menuBtn.addEventListener('click', toggleMenu);
+  navdrawerContainer.addEventListener('click', function (event) {
+    if (event.target.nodeName === 'A' || event.target.nodeName === 'LI') {
+      closeMenu();
+    }
+  });
+})();
